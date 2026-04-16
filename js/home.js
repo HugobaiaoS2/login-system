a4f3b8d4c158eef72cc13618577c0fe69ca16f3e
Hugo <baiaohg@gamil.com>
2026-04-16 16:00:23 -0300

    fix: adiciona redirecionamento para página de registro

--- js/home.js
+++ js/home.js
@@ -3,3 +3,5 @@ const userEmail = localStorage.getItem("userEmail")
 if (userEmail) {
     message.textContent = `Bem-vindo de volta, ${userEmail}`
 }
+
+ window.location.href = "./pages/auth/register.html";
